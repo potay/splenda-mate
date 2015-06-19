6f6fe95398fd9152d8ad89f7a96fa1a25f1a2c84
Turbulous <lleung@bu.edu>
2015-06-19 00:49:36 -0700

    Working on Processing Data

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+/*Declare script flow here:*/
+
+var data = createDummyData();
+data = flattenObject(data);
+getObjectKeysNames(data);
+
 /*
 This segment contains placeholder variable values. 
 Will be removed once node-server is up and runing
@@ -15,8 +21,24 @@ function createDummyData(){
       "internet": 50
     }
   }];
+  return houseMateBills;
 }
 
+function flattenObject(data){
+  return data[0];
+}
 //Debug Here:
-console.log(houseMateBills);
+console.log(data);
+
+//Get Data Keys:
+function getObjectKeysNames(data){
+  var houseMateNames = Object.keys(data);
+  console.log(data);
+  console.log(houseMateNames);
+  for (var i in houseMateNames){
+    console.log(billKeys);
+  }
+
+}
 //Insert array values into index.html here:
+
