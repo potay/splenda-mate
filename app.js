1dc8313d253bec0c56601cc8a373b78e5e8bee77
Turbulous <lleung@bu.edu>
2015-06-19 00:40:22 -0700

    Added Dummy Data

---
+++ app.js
@@ -0,0 +1,22 @@
+/*
+This segment contains placeholder variable values. 
+Will be removed once node-server is up and runing
+*/
+function createDummyData(){
+  var houseMateBills = [{
+    "Lyndon" : {
+      "rent": 850,
+      "utilies": 50,
+      "internet" : 50
+    },
+    "Paultato":{
+      "rent": 500,
+      "utilities": 50,
+      "internet": 50
+    }
+  }];
+}
+
+//Debug Here:
+console.log(houseMateBills);
+//Insert array values into index.html here:
