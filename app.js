efa9f605aaa1a9e0a755da2a3af2ed5601864f9a
Paul Chun <me@paulchun.com>
2015-06-20 20:04:41 -0700

    Update file structure again

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var bills = require('./routes/bills');
+var cashflow = require('./routes/cashflow');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/bills', bills);
+app.use('/cashflow', cashflow);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
